6734a50b8a0c89cd2f15e50e4e14a67321caa983
Kameko <pavlueliss@gmail.com>
2025-04-05 22:43:17 +0200

    Home page

---
+++ frontend/src/api/queries/updateExpenses.ts
@@ -0,0 +1,22 @@
+import {BudgetItem} from "../../types/types";
+
+const UpdateSettings = async (input: {
+  newItems?: BudgetItem[];
+  expectedDatePaycheck?: Date | null;
+}) => {
+  const response = await fetch(`http://localhost:3000/user/settings`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+    body: JSON.stringify(input),
+  });
+  if (!response.ok) {
+    console.log(response);
+    return undefined;
+  }
+  const data = await response;
+  return data;
+};
+export default UpdateSettings;
